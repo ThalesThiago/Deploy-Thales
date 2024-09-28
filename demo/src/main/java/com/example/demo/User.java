3eb9766570a4c12f0413f8794606593dc43d8e58
Thales Dev <thalesbarros7@hotmail.com>
2024-09-28 19:46:30 -0300

    Add Procfile for Heroku deployment

---
+++ demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
