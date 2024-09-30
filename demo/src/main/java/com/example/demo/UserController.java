b3e1ab114a22e311b3e16e9cf11e4892ca7c2b42
ThalesThiago <thalesbarros7@hotmail.com>
2024-09-30 10:14:22 -0300

    delete

--- demo/src/main/java/com/example/demo/UserController.java
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -17,16 +17,4 @@ public class UserController {
     public String helloWorld() {
         return "Hello World!";
     }
-    @Autowired
-    private UserRepository userRepository;
-
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
-    }
 }
